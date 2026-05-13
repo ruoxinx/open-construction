5c0a7e7f1ca7155ddc62394c144b3bfde426aaf3
Ruoxin Xiong <76619793+ruoxinx@users.noreply.github.com>
2026-05-13 00:59:08 -0700

    Update detail.js

--- site/assets/js/detail.js
+++ site/assets/js/detail.js
@@ -455,6 +455,7 @@ function formatLicense(licVal){
     'ODC-BY': 'https://opendatacommons.org/licenses/by/',
     'CC BY-SA 4.0': 'https://creativecommons.org/licenses/by-sa/4.0/',
     'CC BY-NC-ND 3.0': 'https://creativecommons.org/licenses/by-nc-nd/3.0/',
+    'CC BY-NC-ND 4.0': 'https://creativecommons.org/licenses/by-nc-nd/4.0/',
     'AGPL 3.0': 'https://www.gnu.org/licenses/gpl-3.0.html',
     'MIT License with Commons Clause Restriction':'https://github.com/zhu-xlab/GlobalBuildingAtlas/blob/main/LICENSE',
 	'LGPL-3.0':'https://www.gnu.org/licenses/lgpl-3.0.html',
